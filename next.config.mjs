7ba5a3352ffe23533050d9ece8c14c56941c4ae7
hitesh <expertcoder6@gmail.com>
2024-08-09 21:53:25 +0530

    update file

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        forceHttp: true,
-    },
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'https://demo-movie-task.vercel.app/*',
-            },
-        ];
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
